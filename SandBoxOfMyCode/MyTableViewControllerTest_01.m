637628381712e1767b91aeb21e9880ef3fb294b3
chenxxxxxxxxxwang <xing_wang_2012@hotmail.com>
2016-04-22 16:31:17 +0800

    测试tableview
    测试tableview 虚拟数据

--- SandBoxOfMyCode/MyTableViewControllerTest_01.m
+++ SandBoxOfMyCode/MyTableViewControllerTest_01.m
@@ -32,13 +32,11 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return 1;
 }
 
 
