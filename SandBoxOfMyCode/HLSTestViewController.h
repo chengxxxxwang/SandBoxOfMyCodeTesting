b7a2088099c7a12f31af71bedd17a4ba839c1337
chenxxxxxxxxxwang <xing_wang_2012@hotmail.com>
2016-04-23 01:59:44 +0800

    测试HLS
    测试HLS

---
+++ SandBoxOfMyCode/HLSTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  HLSTestViewController.h
+//  SandBoxOfMyCode
+//
+//  Created by chenxingwang on 16/4/23.
+//  Copyright © 2016年 chenxingwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HLSTestViewController : UIViewController
+
+@end
