12b19cf0fb2ef0504c387dbdd18fb0c990c47d08
chenxxxxxxxxxwang <xing_wang_2012@hotmail.com>
2016-04-27 11:47:01 +0800

    测试 本地存储方案 及 二维码生成和存储
    测试 本地存储方案 及 二维码生成和存储
    TMcahe

--- SandBoxOfMyCode/ViewController.m
+++ SandBoxOfMyCode/ViewController.m
@@ -47,14 +47,84 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-    
-    
 }
+
+
+
+
+#pragma mark -
+#pragma mark - 二维码生成及保存
+#pragma mark -
+
 
 - (void)test{
-
     
+    NSString *str= @"itms-apps://itunes.apple.com/us/app/id1048004602?mt=8";//跳转到itunesStore
+    
+    UIImage * image = [self generateQRCode:str width:150.0 height:150.0];
+    
+    UIImageWriteToSavedPhotosAlbum(image, self, @selector(imageSavedToPhotosAlbum: didFinishSavingWithError: contextInfo:), nil);
     
 }
+
+/**
+ *  二维码生成测试
+ *
+ *  @param code   字符串
+ *  @param width  宽
+ *  @param height 高
+ *
+ *  @return UIImage (返回为CIImage 转化为 image后调用,测试可用)
+ */
+
+- (UIImage *)generateQRCode:(NSString *)code width:(CGFloat)width height:(CGFloat)height {
+    
+    // 生成二维码图片
+    CIImage *qrcodeImage;
+    NSData *data = [code dataUsingEncoding:NSISOLatin1StringEncoding allowLossyConversion:false];
+    CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    
+    [filter setValue:data forKey:@"inputMessage"];
+    [filter setValue:@"H" forKey:@"inputCorrectionLevel"];
+    qrcodeImage = [filter outputImage];
+    
+    // 消除模糊
+    CGFloat scaleX = width / qrcodeImage.extent.size.width; // extent 返回图片的frame
+    CGFloat scaleY = height / qrcodeImage.extent.size.height;
+    CIImage *transformedImage = [qrcodeImage imageByApplyingTransform:CGAffineTransformScale(CGAffineTransformIdentity, scaleX, scaleY)];
+    
+    return [self makeUIImageFromCIImage:transformedImage];
+}
+
+- (UIImage *)makeUIImageFromCIImage:(CIImage *)ciImage {
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CGImageRef cgImage = [context createCGImage:ciImage fromRect:[ciImage extent]];
+    
+    UIImage* uiImage = [UIImage imageWithCGImage:cgImage];
+    CGImageRelease(cgImage);
+    
+    return uiImage;
+}
+
+
+- (void)imageSavedToPhotosAlbum:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *) contextInfo {
+    
+    NSString *message;
+    NSString *title;
+    
+    
+    if (!error) {
+        title = @"恭喜";
+        message = @"成功保存到相册";
+    } else {
+        title = @"失败";
+        message = [error description];
+        
+    }
+    /**uialertview desprecated*/
+//    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil];
+//    [alert show];
+}
+
 
 @end
