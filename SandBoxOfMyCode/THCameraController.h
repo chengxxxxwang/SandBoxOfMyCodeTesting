9b35bf920f3377e6367ceac15c29d99d25a51a40
chenxxxxxxxxxwang <xing_wang_2012@hotmail.com>
2016-04-27 16:58:11 +0800

    提交一次
    提交一次

---
+++ SandBoxOfMyCode/THCameraController.h
@@ -0,0 +1,16 @@
+//
+//  THCameraController.h
+//  SandBoxOfMyCode
+//
+//  Created by chenxingwang on 16/4/27.
+//  Copyright © 2016年 chenxingwang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+
+
+@interface THCameraController : AVCaptureSession
+
+@end
---
+++ SandBoxOfMyCode/THCameraController.m
@@ -0,0 +1,13 @@
+//
+//  THCameraController.m
+//  SandBoxOfMyCode
+//
+//  Created by chenxingwang on 16/4/27.
+//  Copyright © 2016年 chenxingwang. All rights reserved.
+//
+
+#import "THCameraController.h"
+
+@implementation THCameraController
+
+@end
